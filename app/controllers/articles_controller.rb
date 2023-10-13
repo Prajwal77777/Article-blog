b100786d61c8419208ebebbd30e46166ff0c62c9
Prajwal77777 <pkascol664@gmail.com>
2023-10-13 19:12:54 +0545

    Refactoring Code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destory]
     def show
-        @articles = Article.find(
-        params[:id]
-        ) 
+         
     end
 
     def index 
@@ -10,7 +9,7 @@ class ArticlesController < ApplicationController
     end
 
     def create 
-        @articles = Article.new(params.require(:article).permit(:title,:description))
+        @articles = Article.new(article_params)
         if @articles.save
             flash[:notice] = "Article was created Sucessfully."
             redirect_to @articles
@@ -24,8 +23,7 @@ class ArticlesController < ApplicationController
     end
 
     def update 
-        @articles = Article.find(params[:id])
-        if @articles.update(params.require(:article).permit(:title, :description))
+        if @articles.update(article_params)
             flash[:notice] = "Article was updated sucessfully"
             redirect_to @articles
         else
@@ -34,13 +32,21 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-        @articles = Article.find(params[:id])
     end
 
     def destroy 
-        @articles = Article.find(params[:id])
         @articles.destroy
         redirect_to articles_path
+    end
+
+    private
+
+    def set_article 
+        @articles = Article.find(params[:id])
+    end
+
+    def article_params 
+        params.require(:article).permit(:title,:description)
     end
 
 end
