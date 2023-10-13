b2b82fd9d0a088a1c1dd2c63169febbd9f4bcaac
Prajwal77777 <pkascol664@gmail.com>
2023-10-13 12:29:22 +0545

    Completion of CRUD APIs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,46 @@
 class ArticlesController < ApplicationController
     def show
-        @article = Article.find(
+        @articles = Article.find(
         params[:id]
         ) 
+    end
+
+    def index 
+        @articles = Article.all
+    end
+
+    def create 
+        @articles = Article.new(params.require(:article).permit(:title,:description))
+        if @articles.save
+            flash[:notice] = "Article was created Sucessfully."
+            redirect_to @articles
+        else 
+            render 'new'
+        end
+    end
+
+    def new 
+        @articles = Article.new
+    end
+
+    def update 
+        @articles = Article.find(params[:id])
+        if @articles.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated sucessfully"
+            redirect_to @articles
+        else
+            render 'edit'
+        end
+    end
+
+    def edit 
+        @articles = Article.find(params[:id])
+    end
+
+    def destroy 
+        @articles = Article.find(params[:id])
+        @articles.destroy
+        redirect_to articles_path
     end
 
 end
